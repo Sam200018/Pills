75caa474cd9b680736adb7940a6fb4023bf348f1
samuel <samuelbaubas@gmail.com>
2021-08-05 00:00:20 -0500

    Se creo el bloc para ver las medicinas, igual modificamos el repo para las medicinas pero se subio en un commit anterior

---
+++ lib/src/medicines/medicine_bloc/medicine_bloc.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+import 'package:pills/respository/medicine/medicine_repository.dart';
+import 'package:pills/respository/medicine/model/medicine.dart';
+part 'medicine_event.dart';
+part 'medicine_state.dart';
+
+class MedicineBloc extends Bloc<MedicineEvent, MedicineState> {
+  final MedicineFirebase _medicinesRepository;
+  StreamSubscription _medicinesSubscription;
+
+  MedicineBloc({MedicineFirebase medicineRepository})
+      : assert(medicineRepository != null),
+        _medicinesRepository = medicineRepository,
+        super(MedicineInitial());
+
+  @override
+  Stream<MedicineState> mapEventToState(
+    MedicineEvent event,
+  ) async* {
+    if (event is LoadMedicines) {
+      yield* _mapLoadMedicinesToState();
+    } else if (event is AddMedicine) {
+      yield* _mapAddMedicineToState(event);
+    } else if (event is UpdateMedicine) {
+      yield* _mapUpdateMedicineToState(event);
+    } else if (event is DeleteMedicine) {
+      yield* _mapDeleteMedicineToState(event);
+    } else if (event is MedicinesUpdated) {
+      yield* _mapMedicinesUpdateToState(event);
+    }
+  }
+
+  Stream<MedicineState> _mapLoadMedicinesToState() async* {
+    _medicinesSubscription?.cancel();
+    _medicinesSubscription = _medicinesRepository
+        .medicines()
+        .listen((medicines) => add(MedicinesUpdated(medicines)));
+  }
+
+  Stream<MedicineState> _mapAddMedicineToState(AddMedicine event) async* {
+    _medicinesRepository.addNewMedicine(event.medicine);
+  }
+
+  Stream<MedicineState> _mapUpdateMedicineToState(
+      UpdateMedicine event) async* {}
+
+  Stream<MedicineState> _mapDeleteMedicineToState(DeleteMedicine event) async* {
+    _medicinesRepository.deleteMedicine(event.medicine);
+  }
+
+  Stream<MedicineState> _mapMedicinesUpdateToState(
+      MedicinesUpdated event) async* {
+    yield MedicinesLoaded(event.medicines);
+  }
+
+  @override
+  Future<void> close() {
+    _medicinesSubscription?.cancel();
+    return super.close();
+  }
+}
