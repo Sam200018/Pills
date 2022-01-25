d2f0f18393bcce5e893a282a0486b422ce2bc2f5
Samuel Bautista <samuelbaubas@gmail.com>
2022-01-25 13:02:17 -0600

    Se esta analizando que mas poner en este ListSide

--- lib/src/utils/UtilsWidgets.dart
+++ lib/src/utils/UtilsWidgets.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'package:pills/src/BLoC/auth/auth_bloc.dart';
 
 class ListSide extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        ListTile(
-          title: Text('Cerrar Sesion'),
-          onTap: () => BlocProvider.of<AuthBloc>(context)
-              .add(AuthenticationLogoutRequested()),
-        )
+        // ListTile(
+        //   title: Text('Cerrar Sesion'),
+        //   onTap: () => BlocProvider.of<AuthBloc>(context)
+        //       .add(AuthenticationLogoutRequested()),
+        // )
       ],
     );
   }
