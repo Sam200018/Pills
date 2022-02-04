9f76b1f1bd248dde4c0774f128313365d9f1dda8
Samuel Bautista <samuelbaubas@gmail.com>
2022-02-04 17:04:52 -0600

    Se modifico el splash Screen para poder utilizarlo como pagina de opciones para iniciar sesion, y utilizamos codigo ya hecho sobre los utils para mostrar cosas como imagenes y que ya sean widgets precargados

--- lib/src/screens/splash_page.dart
+++ lib/src/screens/splash_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:pills/src/login/controller/login_controller.dart';
 
-class SplashScreen extends GetView<LoginController> {
+import 'package:pills/src/login/controller/login_controller.dart';
+import 'package:pills/src/utils/utils.dart';
+
+class SplashScreen extends StatelessWidget {
   static Route route() {
     return MaterialPageRoute(builder: (_) => SplashScreen());
   }
@@ -16,15 +18,98 @@ class SplashScreen extends GetView<LoginController> {
       body: Center(
         child: Column(
           children: [
-            TextButton(
-                onPressed: () => controller.loginWithGoogle(),
-                child: Text('Acceso con google')),
-            SizedBox(height: 30.0),
-            TextButton(
-                onPressed: () => Get.toNamed("/login"),
-                child: Text('Acceso con email')),
+            SizedBox(height: 100),
+            LogoPills(),
+            SizedBox(height: 20.0),
+            GoogleAccessButton(),
+            SizedBox(height: 15.0),
+            EmailAccessButton(),
+            SizedBox(height: 90.0),
+            NewAccountButton(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class GoogleAccessButton extends GetWidget<LoginController> {
+  const GoogleAccessButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 250.0,
+      height: 50.0,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.all(0),
+            primary: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            )),
+        onPressed: () => controller.loginWithGoogle(),
+        child: Row(
+          children: [
+            SizedBox(
+              width: 5.0,
+            ),
+            Image.asset(
+              'assets/logoGoogle.png',
+            ),
+            SizedBox(
+              width: 12.0,
+            ),
+            Text(
+              "Acceso con Google",
+              style: TextStyle(
+                  color: Color.fromRGBO(0, 0, 0, .54),
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w500,
+                  fontFamily: 'Roboto Mono'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class EmailAccessButton extends StatelessWidget {
+  const EmailAccessButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 250,
+      height: 50,
+      child: ElevatedButton(
+          onPressed: () => Get.toNamed("/login"),
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.all(0),
+            primary: buttonText(),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          child: CustomText(
+            textC: 'Acceso con email',
+            size: 20.0,
+          )),
+    );
+  }
+}
+
+class NewAccountButton extends StatelessWidget {
+  const NewAccountButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () => Get.toNamed('/signup'),
+      child: CustomTextUnderline(
+        textC: 'Crear Cuenta',
+        size: 20.0,
       ),
     );
   }
--- lib/src/utils/UtilsWidgets.dart
+++ lib/src/utils/UtilsWidgets.dart
@@ -1,16 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pills/src/Auth/AuthController.dart';
 
-class ListSide extends StatelessWidget {
+class ListSide extends GetWidget<AuthController> {
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        // ListTile(
-        //   title: Text('Cerrar Sesion'),
-        //   onTap: () => BlocProvider.of<AuthBloc>(context)
-        //       .add(AuthenticationLogoutRequested()),
-        // )
+        ListTile(title: Text('Cerrar Sesion'), onTap: () => controller.logOut())
       ],
+    );
+  }
+}
+
+class LogoPills extends StatelessWidget {
+  const LogoPills({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('assets/logo_pillsx1.png');
+  }
+}
+
+class LogoPillsForm extends StatelessWidget {
+  const LogoPillsForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('assets/LogoPillsForm.png');
+  }
+}
+
+class CustomText extends StatelessWidget {
+  final String textC;
+  final double size;
+
+  const CustomText({required this.textC, required this.size});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      textC,
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: size,
+      ),
+    );
+  }
+}
+
+class CustomTextUnderline extends StatelessWidget {
+  final String textC;
+  final double size;
+
+  const CustomTextUnderline({required this.textC, required this.size});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      textC,
+      style: TextStyle(
+        color: Colors.black,
+        decoration: TextDecoration.underline,
+        fontSize: size,
+      ),
     );
   }
 }
